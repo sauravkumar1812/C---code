d053222c84a757eb374427ef3c2a2b73687e589e
saurav18 <shorya181299@gmail.com>
2022-05-26 23:47:55 +0530

    \

---
+++ .vscode/sub2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int sub,num1,num2;
+    cout<<"Enter two number=";
+    cin>>num1>>num2;
+    sub=num1-num2;
+    cout <<"Sub of two number is ="<<sub<<endl;
+    return 0;
+}
