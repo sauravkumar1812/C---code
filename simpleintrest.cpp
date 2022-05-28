865cb0fd3e7a77618c2430ca33b4b47fd40c96ce
saurav18 <shorya181299@gmail.com>
2022-05-28 23:20:11 +0530

    simple intrest

---
+++ simpleintrest.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int p,n;
+    float r,si;
+    cout<<"Enter p ="<<"Enter n ="<<"Enter r =";
+    cin>>p>>n>>r;
+    si=(p*n*r)/100;
+    cout<<"si="<<si<<endl;
+    return 0;
+}
