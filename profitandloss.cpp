9421f02e8e0087213d0514595bfceea66ec42bba
saurav18 <shorya181299@gmail.com>
2022-05-29 15:17:20 +0530

    minnorchange

--- profitandloss.cpp
+++ profitandloss.cpp
@@ -2,15 +2,20 @@
 using namespace std;
 int main(){
     int sp,cp,profit,loss;
+    float profitpercentage,losspercentage;
     cout<<"Enter sellprice ="<<"Enter costprice ="<<endl;
     cin>>sp>>cp;
     if(sp>cp){
         profit =sp-cp;
-        cout<<"profit="<<profit;
+        cout<<"profit="<<profit<<endl;
+        profitpercentage =((sp-cp)*100)/cp;
+        cout<<"profitpercentage ="<<profitpercentage;
     }
 else{
       loss = cp-sp;
-    cout<<"loss="<<loss;
+    cout<<"loss="<<loss<<endl;
+     losspercentage =((cp-sp)*100)/cp;
+        cout<<"losspercentage ="<<losspercentage;
 }
 return 0;
 }
