8d2080e596d4939dbe4064a55c46fb3838bcb9f5
saurav18 <shorya181299@gmail.com>
2022-06-04 02:49:08 +0530

    commit

--- pattern5.cpp
+++ pattern5.cpp
@@ -5,8 +5,12 @@ int main(){
     cout<<"N :";
     cin>>n;
   for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
-          cout<<"*";
+        for(int j=1;j<=i;j++) {
+    if (i==j){
+        cout<<"*";
+    }else{
+        cout<<" ";
+    }
         }
         cout<<endl;
     }
