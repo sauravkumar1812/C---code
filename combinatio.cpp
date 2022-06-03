48b36eff55c4feb44cb6df4304fdecc6670a084f
saurav18 <shorya181299@gmail.com>
2022-06-03 12:13:45 +0530

    code learn

--- combinatio.cpp
+++ combinatio.cpp
@@ -1,14 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int i,j,k;
-    for(i=1;i<=3;i++){
-        for(j=1;j<=3;j++){
-            for(k=1;k<=3;k++){
-                if(i!=j &&j!=k &&k!=i){
-                    cout<<i<<j<<k<<endl;
-                }
-
-        }} }
+    int rows,columns;
+    cout<<"Enter number of rows :"<<endl;
+    Cin>>rows;
+    cout<<"Enter number of columns :"<<endl;
+    Cin>>columns;
+    cout<<rows<<endl<<columns<<endl;
+    for(int i=1;i<=rows;i++){
+        for( int j=1;j<=columns;j++){
+            cout<<"*"<<" ";
+        }
+cout<<endl;
+    }
     return 0;
 }
--- pattern1.cpp
+++ pattern1.cpp
@@ -1,5 +1,16 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a,b
+    int rows,columns;
+    cout<<"Enter the number of rows : "<<endl;
+    cin>>rows;
+    cout<<"Enter the number of columns :"<<endl;
+    cin>>columns;
+    for(int i=1;i<=rows;i++){
+        for(int j=1;j<=columns;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
 }
