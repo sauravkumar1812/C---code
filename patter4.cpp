52c6794cc0c46a53cce1ab5c31a7aaabd5e0150c
saurav18 <shorya181299@gmail.com>
2022-06-03 12:41:44 +0530

    code learn

---
+++ patter4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int i,j,n;
+    cout<<"N : ";
+    cin>>n;
+    for(i=n;i>=1;i--){
+      for(j=1;j<=i;j++){
+        cout<<"*";
+    }  
+    cout<<endl;
+    }
+    return 0;
+}
