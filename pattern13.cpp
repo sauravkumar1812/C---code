0ab7913b71bb59ff48b21dda9a2b716a1fd1c867
saurav18 <shorya181299@gmail.com>
2022-06-05 02:33:13 +0530

    comit

---
+++ pattern13.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int n;
+    cout<<"N :";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i+1;++j){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
---
+++ pattern16.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"N :";
+    cin>>n;
+   
+    int count=-0;
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=count;j++){
+            cout<<"  ";
+
+        }
+        for(int k=1;k<=(2*n-1);k++){
+            cout<<"*";
+        }
+        cout<<endl;
+        count++;
+        
+    }
+    return 0;
+}
