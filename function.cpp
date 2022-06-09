cb52aa3812068208e1caf2a224e2bf10328e10f5
saurav18 <shorya181299@gmail.com>
2022-06-09 22:40:00 +0530

    function code

---
+++ function.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+void lcm(int x,int y)
+{
+    x= x+y;
+}
+int main()
+{
+    int a, b;
+   cout<<"this is main function" ; 
+    cin>>a>>b;
+    lcm(a,b);
+    cout<<a;
+}
+
