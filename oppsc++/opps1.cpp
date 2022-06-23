19ae58311f61fd0e07253576121c0b4ead1fb8e8
saurav18 <shorya181299@gmail.com>
2022-06-24 00:40:07 +0530

    intial commit

---
+++ oppsc++/opps1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<>
+using namespace std;
+
+class classname{
+    public:
+    int var; 
+    int a;
+};
+class student{
+    public:
+    
+
+
+};
+
+int main(){
+    int var;
+    classname obj;
+    obj.a=15;
+    obj.var=10;
+    cout<<obj.var<<"  "<<obj.a; 
+    return 0;
+}
---
+++ oppsc++/opps2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=0,b=1,sum,temp;
+    cout<<"Enter two number :"<<endl;
+    cin>>a>>b;
+    sum=a+b;
+    cout<<"sum ="<<sum;
+    a=b;
+    b=temp;
+    return 0;
+}
