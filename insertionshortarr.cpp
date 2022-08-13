d6e4a156eec2670c36ad295af35870e5c612873b
saurav18 <shorya181299@gmail.com>
2022-08-13 14:26:23 +0530

    normal

---
+++ insertionshortarr.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n,arr[100];
+cout<<"Enter size of arrays :";
+cin>>n;
+for(int i=0;i<n;i++){
+    cin>>arr[i];
+}
+for (int j=1;j<n;j++){
+   int curr=arr[j];
+    int k=j-1;
+    while(arr[k]>curr && k>=0){
+        arr[k+1]=arr[k];
+        k--;
+    }
+    arr[k+1]=curr;
+}
+for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+}
+return 0;
+}
