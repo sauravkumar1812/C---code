326ae6f5564eda2ea9919746a2d5330070a72c12
saurav18 <shorya181299@gmail.com>
2022-08-18 23:07:18 +0530

    normal

---
+++ string.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+// char str[20];
+// cin>>str;
+string str;
+getline(cin,str);
+// string a;
+cout<<str;
+    return 0;
+}
