0d18fd3b1cb2e6273f19e3fbdb21f4ecf932743b
saurav18 <shorya181299@gmail.com>
2022-08-19 22:51:29 +0530

    new code

---
+++ palindromeinstring.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str;
+    bool ispal=true;
+    cout<<"Enter a string :";
+    cin>>str;
+    cout<<str<<endl;
+    int i=0;
+    int j=str.length()-1;
+    while (i<=j)
+    {
+       if(str[i]!=str[j]){
+          ispal=false;
+        // cout<<"Not Palindrome!"<<endl;
+         break;
+       }
+       i++;
+       j--;
+    }
+    if(ispal==true){
+        cout<<"PALINDROME"<<endl;
+    }
+    else{
+        cout<<"NOT PALINDROME"<<endl;
+    }
+    
+    return 0;
+}
