e7eee210f1a8fab4f743266c1a73d337dfdcbf92
saurav18 <shorya181299@gmail.com>
2022-08-23 23:05:29 +0530

    new code

---
+++ stringbasic1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    const char*  name[100]={"Saurav","AYUSH","VISHAL","ABHIJEET"};
+    for(int i=0;i<4;i++){
+    cout<<"NAME :"<<name[i]<<endl;
+    }
+    return 0;
+}
